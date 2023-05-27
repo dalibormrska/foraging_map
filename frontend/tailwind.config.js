dc1221255328dabfa58433341e5ffa907e335136
Zoe Zhiqian Huang <68361445+glassetsmi@users.noreply.github.com>
2023-05-27 22:17:19 +0200

    Installed tailwind.exe, changed fonts/logo

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,10 +13,10 @@ module.exports = {
     //   'gray': '#8492a6',
     //   'gray-light': '#d3dce6',
     // },
-    // fontFamily: {
-    //   sans: ['Graphik', 'sans-serif'],
-    //   serif: ['Merriweather', 'serif'],
-    // },
+    fontFamily: {
+      sans: ['axia', 'sans-serif'],
+      // serif: ['', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
