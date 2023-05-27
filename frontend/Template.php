9bd3e43a471c483a7577142c2cdcb2217f65c8c1
Zoe Zhiqian Huang <68361445+glassetsmi@users.noreply.github.com>
2023-05-27 21:36:28 +0200

    Changed logo, tried to add fonts

--- frontend/Template.php
+++ frontend/Template.php
@@ -14,7 +14,8 @@ class Template
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <script src="https://cdn.tailwindcss.com"></script>
-
+            <!-- Adobe Font -->
+            <link rel="stylesheet" href="https://use.typekit.net/dxm1csy.css">
             <!-- Leaflet CSS -->
             <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
 
@@ -38,11 +39,11 @@ class Template
             <nav class="bg-green-800 py-4 drop-shadow-xl">
                 <div class="container mx-auto flex justify-between items-center">
                     <div class="flex items-center rounded-lg">
-                        <span class="text-orange-50 font-bold text-xl">Foraging Map</span>
+                    <img src= "<?= $home_path ?>/assets/img/logo.png" class="h-8 mr-3" alt="Flowbite Logo" />
                     </div>
                     <div class="flex items-center">
-                        <a href="#" class="text-orange-50 mr-8 hover:underline underline-offset-4 ">Sign Up</a>
-                        <a href="#" class="text-orange-50 border border-orange-50 rounded-full px-4 py-2 hover:bg-orange-50 hover:text-green-800">Login</a>
+                        <a href="#" class="text-white mr-8 hover:underline underline-offset-4 ">Sign Up</a>
+                        <a href="#" class="text-white border border-white rounded-full px-4 py-2 hover:bg-white hover:text-green-800">Login</a>
                     </div>
                 </div>
             </nav>
